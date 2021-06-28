bb9d20c9ae63a03f65a625f7cc2d018347321577
Krista Marbury <laetaiustus@gmail.com>
2021-06-28 17:23:54 -0500

    product tag class file updated

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,30 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+        // is it true or false?
+        unique: false,
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+        // is it true or false?
+        unique: false,
+      },
+    },
   },
   {
     sequelize,
