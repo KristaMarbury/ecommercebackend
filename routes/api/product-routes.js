9a91eb72385b22162312830d1bc6d3bed2026cfe
Krista Marbury <laetaiustus@gmail.com>
2021-06-29 14:13:17 -0500

    updated product routes file

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -14,7 +14,20 @@ router.get("/", (req, res) => {
 router.get("/:id", (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ models: Category, Tag }],
+    });
 
+    if (!productData) {
+      res.status(404).json({ message: "Nothing found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -93,7 +106,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
-  
+    try {
+      const productData = await Product.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      if (!productData) {
+        res.status(404).json({ message: "Nothing found with this id!" });
+        return;
+      }
+
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
