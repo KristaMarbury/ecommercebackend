a2e5b26c7f0e03d0a35de1be1d12814ccff8dc33
Krista Marbury <laetaiustus@gmail.com>
2021-07-03 15:49:09 -0500

    trying to make the post work in product route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -56,6 +56,44 @@ router.post("/", async (req, res) => {
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  // update category data
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((category) => {
+      // find all associated tags from ProductTag
+      return Category.findAll({ where: { category_id: req.params.id } });
+    })
+    .then((category) => {
+      // get list of current tag_ids
+      const categoryIds = productTags.map(({ tag_id }) => tag_id);
+      // create filtered list of new tag_ids
+      const newProductTags = req.body.tagIds
+        .filter((tag_id) => !productTagIds.includes(tag_id))
+        .map((tag_id) => {
+          return {
+            product_id: req.params.id,
+            tag_id,
+          };
+        });
+      // figure out which ones to remove
+      const productTagsToRemove = productTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
+
+      // run both actions
+      return Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    })
+    .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete("/:id", async (req, res) => {
