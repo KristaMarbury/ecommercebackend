0243531e2552c09e8788f883bbff6b5760159003
Krista Marbury <laetaiustus@gmail.com>
2021-07-02 19:22:39 -0500

    fmade findall work in every route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,6 +9,12 @@ router.get("/", async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll();
+
+    if (!productData) {
+      res.status(404).json({ message: "Nothing found with this id!" });
+      return;
+    }
+
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -111,22 +117,22 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
-    try {
-      const productData = await Product.destroy({
-        where: {
-          id: req.params.id,
-        },
-      });
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
 
-      if (!productData) {
-        res.status(404).json({ message: "Nothing found with this id!" });
-        return;
-      }
-
-      res.status(200).json(productData);
-    } catch (err) {
-      res.status(500).json(err);
+    if (!productData) {
+      res.status(404).json({ message: "Nothing found with this id!" });
+      return;
     }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,20 +6,18 @@ const { Tag, Product, ProductTag } = require("../../models");
 router.get("/", async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
- try {
-   const tagData = await Tag.findByPk(req.params.id, {
-     include: [{ model: Product, through: ProductTag }],
-   });
+  try {
+    const tagData = await Tag.findAll();
 
-   if (!categoryData) {
-     res.status(404).json({ message: "Nothing found with this id!" });
-     return;
-   }
+    if (!tagData) {
+      res.status(404).json({ message: "Nothing found with this id!" });
+      return;
+    }
 
-   res.status(200).json(tagData);
- } catch (err) {
-   res.status(500).json(err);
- }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get("/:id", async (req, res) => {
@@ -43,17 +41,16 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   // create a new tag
-   try {
-     const tagData = await Tag.create(req.body);
-     res.status(200).json(tagData);
-   } catch (err) {
-     res.status(400).json(err);
-   }
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
-
 });
 
 router.delete("/:id", async (req, res) => {
